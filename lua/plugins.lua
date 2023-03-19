14b89b6c180e03e1815c770169a308b4d50568c4
Jesús S.P <shamandul@gmail.com>
2023-03-19 11:05:19 +0100

    inicia con una config basica

---
+++ lua/plugins.lua
@@ -0,0 +1,33 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  	-- Packer can manage itself
+  	use 'wbthomason/packer.nvim'
+  	use {
+ 	 'nvim-telescope/telescope.nvim',
+	 requires = {{'nvim-lua/plenary.nvim'}}
+        }
+	use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+        } 
+	use 'nvim-treesitter/playground'
+	use {
+    	   "williamboman/nvim-lsp-installer",
+    	   "neovim/nvim-lspconfig",
+	}
+  
+
+end
+)
+
+
